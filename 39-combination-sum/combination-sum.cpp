8937f48fa673b401143bc2dbc9501ac9dc15025d
Aditya Singh <95461393+TOJIZENN@users.noreply.github.com>
2025-06-30 18:47:43 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (90.85%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,20 +1,30 @@
 class Solution {
 public:
-void solve(int i,vector<int>& candidates,vector<vector<int>>&ans,vector<int>temp,int target,int curr)
-{
-    if(curr==target){ans.push_back(temp);return;}
-    if(i>=candidates.size() || curr>target){return;}
-    temp.push_back(candidates[i]);
-    solve(i,candidates,ans,temp,target,curr+candidates[i]);
-    temp.pop_back();
-    solve(i+1,candidates,ans,temp,target,curr);
-}
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) 
-    {
-        vector<vector<int>>ans;
-        vector<int>temp;
-        int curr=0;
-        solve(0,candidates,ans,temp,target,curr);
+    void solve(vector<int>& candidates, int target, int i, vector<int>& temp, vector<vector<int>>& ans, int& tval, int n) {
+        if (tval == target) {
+            ans.push_back(temp);
+            return;
+        }
+
+        if (tval > target || i > n) return;
+
+        // pick current number
+        temp.push_back(candidates[i]);
+        tval += candidates[i];
+        solve(candidates, target, i, temp, ans, tval, n);
+        tval -= candidates[i];       // backtrack tval
+        temp.pop_back();             // backtrack temp
+
+        // don't pick current number
+        solve(candidates, target, i + 1, temp, ans, tval, n);
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        int n = candidates.size() - 1;
+        vector<vector<int>> ans;
+        vector<int> temp;
+        int tval = 0;
+        solve(candidates, target, 0, temp, ans, tval, n);
         return ans;
     }
 };
