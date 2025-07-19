a253beaf16d59347475eaa784e27b8b0853408e5
Aditya Singh <95461393+TOJIZENN@users.noreply.github.com>
2025-07-19 22:07:42 +0530

    Time: 439 ms (28.21%) | Memory: 41.7 MB (39.61%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n+1, -1));
-
-        function<int(int, int)> solve = [&](int idx, int prevIdx) -> int {
-            if (idx == n) return 0;
-
-            if (dp[idx][prevIdx + 1] != -1) return dp[idx][prevIdx + 1];
-
-            int notTake = solve(idx + 1, prevIdx); // skip current
-            int take = 0;
-
-            if (prevIdx == -1 || nums[idx] > nums[prevIdx]) {
-                take = 1 + solve(idx + 1, idx); // take current
-            }
-
-            return dp[idx][prevIdx + 1] = max(take, notTake);
-        };
-
-        return solve(0, -1);
+int n;
+int dp[2501][2501];
+   int solve(int i,int prev,vector<int>&nums)
+     {
+        if(i>=n){return 0;}
+        if(prev!=-1&&dp[i][prev]!=-1){return dp[i][prev];}
+        int take=0;
+        if(prev==-1 || nums[prev]<nums[i])
+        {
+          take=1+solve(i+1,i,nums);
+        }  
+       int skip=solve(i+1,prev,nums);
+        if(prev!=-1){dp[i][prev]=max(take,skip);}
+        return max(take,skip);
+     }
+    int lengthOfLIS(vector<int>& nums)
+    {
+        n=nums.size();
+        memset(dp,-1,sizeof(dp));
+         return solve(0,-1,nums);
     }
 };
