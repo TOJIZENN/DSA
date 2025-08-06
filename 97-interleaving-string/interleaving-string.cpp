422f2116a0a70b5d407d710c13d2e1790d22b819
Aditya Singh <95461393+TOJIZENN@users.noreply.github.com>
2025-08-06 22:19:44 +0530

    Time: 21 ms (22.86%) | Memory: 25.2 MB (21.26%) - LeetSync

---
+++ 97-interleaving-string/interleaving-string.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+int n1,n2,n3;
+int dp[500][500];
+bool solve(int i,int j,int k,string s1, string s2, string s3)
+{
+if(i==n1 && j==n2 && k==n3){return true;}
+if(dp[i][j]!=-1){return dp[i][j];}
+if(i < s1.size()&&s1[i]==s3[k]&&s2[j]!=s3[k])
+{
+return dp[i][j]=(solve(i+1,j,k+1,s1,s2,s3));
+}
+else if(j < s2.size()&&s1[i]!=s3[k]&&s2[j]==s3[k])
+{
+return dp[i][j]=solve(i,j+1,k+1,s1,s2,s3);
+}
+else if(s1[i]==s3[k]&&s2[j]==s3[k])
+{
+    return dp[i][j]=(solve(i+1,j,k+1,s1,s2,s3)||solve(i,j+1,k+1,s1,s2,s3));
+}
+return dp[i][j]=false;
+}
+    bool isInterleave(string s1, string s2, string s3) 
+    {
+        memset(dp,-1,sizeof(dp));
+        if(s1.length() + s2.length() != s3.length()){return false;}
+        if(!s1.empty() && s2.empty() && s3!=s1){return false;}
+        if(!s2.empty() && s1.empty() && s3!=s2){return false;}
+        n1=s1.size();
+        n2=s2.size();
+        n3=s3.size();
+        return solve(0,0,0,s1,s2,s3);
+    }
+};
