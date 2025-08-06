c7b65ad3d98203ea165aa74e0c73793587e3caf4
Aditya Singh <95461393+TOJIZENN@users.noreply.github.com>
2025-08-06 22:20:22 +0530

    Time: 18 ms (24.58%) | Memory: 25 MB (21.44%) - LeetSync

--- 97-interleaving-string/interleaving-string.cpp
+++ 97-interleaving-string/interleaving-string.cpp
@@ -6,11 +6,11 @@ bool solve(int i,int j,int k,string s1, string s2, string s3)
 {
 if(i>=n1 && j>=n2 && k>=n3){return true;}
 if(dp[i][j]!=-1){return dp[i][j];}
-if(i < s1.size()&&s1[i]==s3[k]&&s2[j]!=s3[k])
+if(s1[i]==s3[k]&&s2[j]!=s3[k])
 {
 return dp[i][j]=(solve(i+1,j,k+1,s1,s2,s3));
 }
-else if(j < s2.size()&&s1[i]!=s3[k]&&s2[j]==s3[k])
+else if(s1[i]!=s3[k]&&s2[j]==s3[k])
 {
 return dp[i][j]=solve(i,j+1,k+1,s1,s2,s3);
 }
