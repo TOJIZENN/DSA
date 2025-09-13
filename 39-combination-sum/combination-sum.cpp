cb387777267c29ff12cd5f594017f10c7afb889f
Aditya Singh <95461393+TOJIZENN@users.noreply.github.com>
2025-09-13 21:32:45 +0530

    Time: 39 ms (17.50%) | Memory: 42.7 MB (15.18%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
-    void solve(vector<int>& candidates, int target, int i, vector<int>& temp, vector<vector<int>>& ans, int& tval, int n) {
-        if (tval == target) {
-            ans.push_back(temp);
-            return;
-        }
-
-        if (tval > target || i > n) return;
-
-        // pick current number
+vector<vector<int>>ans;
+void solve(int i,vector<int>& candidates, int target,vector<int>temp)
+{
+    if(target==0){ans.push_back(temp);return;}
+    if(i>=candidates.size() || target<0){return;}
+    if(target-candidates[i]>=0)
+    {
         temp.push_back(candidates[i]);
-        tval += candidates[i];
-        solve(candidates, target, i, temp, ans, tval, n);
-        tval -= candidates[i];       // backtrack tval
-        temp.pop_back();             // backtrack temp
-
-        // don't pick current number
-        solve(candidates, target, i + 1, temp, ans, tval, n);
+        solve(i,candidates,target-candidates[i],temp);
+        temp.pop_back();
     }
-
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        int n = candidates.size() - 1;
-        vector<vector<int>> ans;
-        vector<int> temp;
-        int tval = 0;
-        solve(candidates, target, 0, temp, ans, tval, n);
+    
+    solve(i+1,candidates,target,temp);
+}
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) 
+    {
+        vector<int>temp;
+        solve(0,candidates,target,temp);
         return ans;
     }
 };
