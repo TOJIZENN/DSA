572a01e689b90d8f78cd4b183e13890b33ad1d37
Aditya Singh <95461393+TOJIZENN@users.noreply.github.com>
2025-11-26 22:32:54 +0530

    Time: 0 ms (100.00%) | Memory: 34.3 MB (38.66%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
@@ -6,32 +6,36 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    int find(TreeNode* root) {
-        while (root->left) {
-            root = root->left;
-        }
-        return root->val;
-    }
+    TreeNode* solve(TreeNode* root, int key)
+    {
+        if(!root){return nullptr;}
+          if(key>root->val)
+          {
+            root->right=solve(root->right,key);
+          }
+         else if(key<root->val){root->left=solve(root->left,key);}
+          else
+          {
+            if (!root->left && !root->right) return nullptr;
+            if (!root->left) return root->right;
+            if (!root->right) return root->left;  
+            root->val=find(root->right);
+            root->right=solve(root->right,root->val);
+          }
+          return root;
+    } 
+     int find(TreeNode* root)
+     {
+       while(root->left){root=root->left;}
+       return root->val;
+     } 
     TreeNode* deleteNode(TreeNode* root, int key) 
     {
-       if(!root){return nullptr;}
-       if(key<root->val){root->left=deleteNode(root->left,key);}
-      else if(key>root->val){root->right=deleteNode(root->right,key);}
-      else
-      {
-        if(!root->left && !root->right){return nullptr;}
-        if(!root->left){return root->right;}
-        if(!root->right){return root->left;}
-        root->val=find(root->right);
-        root->right=deleteNode(root->right,root->val);
-      }
- return root;
-
+     return solve(root,key);   
     }
-    };
+};
