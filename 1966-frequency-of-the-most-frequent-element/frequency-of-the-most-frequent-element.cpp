cfe77f11abdf571e88d1fc5b3f6543c915cf501a
Aditya Singh <95461393+TOJIZENN@users.noreply.github.com>
2025-12-01 22:51:30 +0530

    Time: 43 ms (40.56%) | Memory: 105.8 MB (91.97%) - LeetSync

--- 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    int maxFrequency(vector<int>& nums, int k) 
+    int maxFrequency(vector<int>& nums, int k)
     {
-        ios_base::sync_with_stdio(false);cin.tie(NULL);
         sort(nums.begin(),nums.end());
-        long long i=0,j=0;
-        long n=nums.size();
-        long long ans=0;
         long long prefixsum=0;
+        long long j=0;
+        long long ans=INT_MIN;
+        long long i=0;
+        long long n=nums.size();
         while(j<n)
         {
             prefixsum+=nums[j];
             while(nums[j]*(j-i+1)>prefixsum+k)
             {
-               prefixsum-=nums[i];
-               i++;
+                prefixsum-=nums[i];
+                i++;
             }
-            ans=max(ans,j-i+1);
-            j++;
+        ans=max(ans,j-i+1);
+        j++;
         }
         return ans;
     }
