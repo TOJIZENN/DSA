54438deef5fbdcb8127f51d2aecd9e2931f21ec7
Aditya Singh <95461393+TOJIZENN@users.noreply.github.com>
2026-01-21 23:09:58 +0530

    Time: 0 ms (100.00%) | Memory: 34.3 MB (71.38%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
@@ -6,36 +6,44 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    TreeNode* solve(TreeNode* root, int key)
-    {
-        if(!root){return nullptr;}
-          if(key>root->val)
-          {
-            root->right=solve(root->right,key);
-          }
-         else if(key<root->val){root->left=solve(root->left,key);}
-          else
-          {
-            if (!root->left && !root->right) return nullptr;
-            if (!root->left) return root->right;
-            if (!root->right) return root->left;  
-            root->val=find(root->right);
-            root->right=solve(root->right,root->val);
-          }
-          return root;
-    } 
-     int find(TreeNode* root)
-     {
-       while(root->left){root=root->left;}
-       return root->val;
-     } 
-    TreeNode* deleteNode(TreeNode* root, int key) 
-    {
-     return solve(root,key);   
+    TreeNode* solve(TreeNode* root, int key) {
+
+        if (!root) {
+            return nullptr;
+        }
+        else if (root->val > key) {
+            root->left = solve(root->left, key);
+        }
+        else if (root->val < key) {
+            root->right = solve(root->right, key);
+        } else {
+            if (!root->left && !root->right) {
+                return nullptr;
+            }
+            else  if (!root->left) {
+                return root->right;
+            }
+            else if (!root->right) {
+                return root->left;
+            }
+            root->val = find(root->right);
+            root->right = solve(root->right, root->val);
+        }
+        return root;
     }
+
+    int find(TreeNode* root) {
+        while (root->left) {
+            root=root->left;
+        }
+        return root->val;
+    }
+    TreeNode* deleteNode(TreeNode* root, int key) { 
+        return solve(root, key); }
 };
